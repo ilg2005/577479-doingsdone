9aa9b2aac0fd52f551a46c65761d5445344be100
ilg2005 <igoldfarb10@gmail.com>
2019-02-05 22:31:23 +0300

    вместо содержимого тега main вывод $mainContent

--- templates/layout.php
+++ templates/layout.php
@@ -58,53 +58,7 @@
             </section>
 
             <main class="content__main">
-                <h2 class="content__main-heading">Список задач</h2>
-
-                <form class="search-form" action="index.php" method="post">
-                    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
-
-                    <input class="search-form__submit" type="submit" name="" value="Искать">
-                </form>
-
-                <div class="tasks-controls">
-                    <nav class="tasks-switch">
-                        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-                        <a href="/" class="tasks-switch__item">Повестка дня</a>
-                        <a href="/" class="tasks-switch__item">Завтра</a>
-                        <a href="/" class="tasks-switch__item">Просроченные</a>
-                    </nav>
-
-                    <label class="checkbox">
-                        <input class="checkbox__input visually-hidden show_completed"
-                               type="checkbox" <?= ($show_complete_tasks === 1) ? 'checked' : ''; ?>>
-                        <span class="checkbox__text">Показывать выполненные</span>
-                    </label>
-                </div>
-
-                <table class="tasks">
-                    <?php foreach ($tasks as $k => $v): ?>
-                        <?php if ($v['isDone'] && !$show_complete_tasks) {
-                            continue;
-                        } ?>
-                        <tr class="tasks__item task <?= $v['isDone'] ? 'task--completed' : ''; ?>">
-                            <td class="task__select">
-                                <label class="checkbox task__checkbox">
-                                    <input class="checkbox__input visually-hidden task__checkbox" type="checkbox"
-                                        <?= ($v['isDone'] && $show_complete_tasks === 1) ? 'checked' : ''; ?>>
-                                    <span class="checkbox__text"><?= $v['title']; ?></span>
-                                </label>
-                            </td>
-
-                            <!--                        <td class="task__file">-->
-                            <!--                            <a class="download-link" href="#"></a>-->
-                            <!--                        </td>-->
-
-                            <td class="task__date"><?= $v['implementationDate']; ?></td>
-                            <td class="task__controls">
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
+                <?= $mainContent ?>
             </main>
         </div>
     </div>
