0d34b2602cf1d05df61d5ebfeb543b6bd0bd5da6
ilg2005 <igoldfarb10@gmail.com>
2019-02-06 08:59:10 +0300

    создан и подключен config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = [
+    'pageTitle' => 'Дела в порядке',
+    'templatePath' => 'templates/',
+    // сайт доступен и работает
+    'enable' => true
+];
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require_once('functions.php');
+require_once('config.php');
+require_once('data.php');
+
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
