0079aa4b689c80a55abf9501ae15e663a600a003
ilg2005 <igoldfarb10@gmail.com>
2019-02-06 09:02:56 +0300

    создан и подключен data.php

--- index.php
+++ index.php
@@ -6,46 +6,6 @@ require_once('data.php');
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
-$projects = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
-$tasks = [
-    [
-        'title' => 'Собеседование в IT компании',
-        'implementationDate' => '01.12.2019',
-        'projectCategory' => 'Работа',
-        'isDone' => false
-    ],
-    [
-        'title' => 'Сделать задание первого раздела',
-        'implementationDate' => '21.12.2019',
-        'projectCategory' => 'Учеба',
-        'isDone' => true
-    ],
-    [
-        'title' => 'Выполнить тестовое задание',
-        'implementationDate' => '25.12.2019',
-        'projectCategory' => 'Работа',
-        'isDone' => false
-    ],
-    [
-        'title' => 'Встреча с другом',
-        'implementationDate' => '22.12.2019',
-        'projectCategory' => 'Входящие',
-        'isDone' => false
-    ],
-    [
-        'title' => 'Купить корм для кота',
-        'implementationDate' => 'Нет',
-        'projectCategory' => 'Домашние дела',
-        'isDone' => false
-    ],
-    [
-        'title' => 'Заказать пиццу',
-        'implementationDate' => 'Нет',
-        'projectCategory' => 'Домашние дела',
-        'isDone' => false
-    ]
-];
-
 $countTasks4Projects = function ($tasksArray, $projectName) {
     $count = 0;
     foreach ($tasksArray as $item) {
