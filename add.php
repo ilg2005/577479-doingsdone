98d71c8d2a65998f490d0025749b8f7f4e429825
ilg2005 <igoldfarb10@gmail.com>
2019-02-24 19:18:46 +0300

    шаблон подключен, layout отрисовывается правильно, проекты заполнены записями из БД

--- add.php
+++ add.php
@@ -1,3 +1,28 @@
 <?php
+require_once('mysql_helper.php');
+require_once('functions.php');
 
+$connection = connect2Database('localhost', 'root', '', 'doingsdone');
+
+$userID = 4;
+$userData = isUserExist($connection, $userID);
+
+if ($connection && $userData) {
+    $userName = $userData['name'];
+    $projects = getProjects($connection, $userData['id']);
+} else {
+    die('Произошла ошибка!');
+}
+
+$mainContent = includeTemplate('add.php', [
+]);
+
+$layout = includeTemplate('layout.php', [
+    'pageTitle' => $pageTitle,
+    'userName' => $userName,
+    'projects' => $projects,
+    'mainContent' => $mainContent
+]);
+
+print($layout);
 ?>
