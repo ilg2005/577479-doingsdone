574833783a49669603a69509fab182626b5d95ce
ilg2005 <igoldfarb10@gmail.com>
2019-03-01 02:46:29 +0300

    статус задачи обновлияется в БД после клика по чекбоксу

--- templates/index.php
+++ templates/index.php
@@ -35,23 +35,26 @@
         ">
             <td class="task__select">
                 <label class="checkbox task__checkbox">
-                    <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" name="task_id" value="<?= $task['id']?>"
-
+                    <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" name="task_id"
+                           value="<?= $task['id'] ?>"
+                        <?php if (isset($_GET['task_id'])) : ?>
+                            <?php changeTaskStatusInDatabase(); ?>
+                        <?php endif; ?>
                         <?php if ($task['is_done'] && $show_complete_tasks === 1) : ?>
                             checked
                         <?php endif; ?>>
                     <span class="checkbox__text"><?= $task['name']; ?></span>
                 </label>
             </td>
-        <?php if($task['file_name']) : ?>
-            <td class="task__file">
-                <a class="download-link" href="<?= '/doingsdone/' . $task['file_name']; ?>"></a>
-            </td>
+            <?php if ($task['file_name']) : ?>
+                <td class="task__file">
+                    <a class="download-link" href="<?= '/doingsdone/' . $task['file_name']; ?>"></a>
+                </td>
             <?php else : ?>
-            <td></td>
-        <?php endif; ?>
+                <td></td>
+            <?php endif; ?>
 
-            <td class="task__date"><?= ($task['deadline'] != 0)? $task['deadline'] : ''; ?>
+            <td class="task__date"><?= ($task['deadline'] != 0) ? $task['deadline'] : ''; ?>
             </td>
 
             <td class="task__controls">
