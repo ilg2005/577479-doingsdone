8b344ac8a116ad88a6928575388b942dfd24cc5b
ilg2005 <igoldfarb10@gmail.com>
2019-03-03 15:28:07 +0300

    почта отправляется

---
+++ email.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'functions.php';
+
+$user['name'] = " ";
+$task['name'] = "Отладить отправку почты";
+$task['deadline'] = "сегодня";
+
+$subject = "Уведомление от сервиса «Дела в порядке»";
+$messageBody = "Уважаемый, " . $user['name'] . "!\nУ вас запланирована задача " . $task['name'] . " на " . $task['deadline'];
+
+try {
+// Конфигурация траспорта
+    $transport = (new Swift_SmtpTransport('smtp.mail.ru', 465, 'ssl'))
+        ->setUsername('igor_test@list.ru')
+        ->setPassword('new_123');
+
+// Формирование сообщения
+    $message = new Swift_Message($subject);
+    $message->setTo(['igor_test@list.ru' => 'Игорь']);
+    $message->setBody($messageBody);
+    $message->setFrom('igor_test@list.ru', 'Игорь');
+
+// Отправка сообщения
+    $mailer = new Swift_Mailer($transport);
+    $mailer->send($message);
+} catch (Exception $error) {
+    echo $error->getMessage();
+}
+?>
